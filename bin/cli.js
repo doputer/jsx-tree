baf8d191ed93ef84fc482d326e1e7635ee4dba50
김도현 <swputer@gmail.com>
2025-04-24 21:28:19 +0900

    feat: CLI 명령어 수정
    - argument path 추가
    - default path 확장자 추가

--- bin/cli.js
+++ bin/cli.js
@@ -2,11 +2,12 @@
 
 const fs = require('node:fs');
 const path = require('node:path');
-const chalk = require('chalk');
+const { yellow } = require('chalk');
 const { program } = require('commander');
 
 const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8'));
-const error = console.error;
+const log = console.log;
+const warn = message => console.log(yellow(message));
 
 const logo = `
      ██╗███████╗██╗  ██╗    ████████╗██████╗ ███████╗███████╗
@@ -22,18 +23,36 @@ program
   .description(pkg.description)
   .version(pkg.version)
   .addHelpText('beforeAll', logo)
-  .option('-e, --entry <file>', 'entry file to analyze (default: ./index.jsx)');
+  .argument('[file]', 'Path to the entry file (optional if using -e or default file exists)')
+  .option('-e, --entry <file>', 'Entry file to analyze (default: ./index.jsx or ./index.tsx)');
 
-const options = program.parse(process.argv).opts();
+program.action((entryArg, options) => {
+  const cwd = process.cwd();
+  const entry = options.entry || entryArg || getDefaultEntry(cwd);
 
-const entryPath = options.entry
-  ? path.resolve(process.cwd(), options.entry)
-  : path.resolve(process.cwd(), 'index.jsx');
+  if (!entry) {
+    log('error: the specified entry file does not exist');
+    warn('Please provide a valid entry file using the -e option or positional argument.');
+    process.exit(1);
+  }
 
-if (!entryPath || !fs.existsSync(entryPath)) {
-  error(`${chalk.yellow('Entry file not found: ')}${entryPath}`);
-  error(chalk.red(`Please provide a valid path using the -e option.`));
-  process.exit(1);
+  if (!fs.existsSync(entry)) {
+    log(`error: the specified entry file does not exist: ${entry}`);
+    warn('Please check the file path and try again.');
+    process.exit(1);
+  }
+
+  require('../dist/core/analyzer.js').default(entry);
+});
+
+program.parse(process.argv);
+
+function getDefaultEntry(dir) {
+  const jsxPath = path.resolve(dir, 'index.jsx');
+  const tsxPath = path.resolve(dir, 'index.tsx');
+
+  if (fs.existsSync(jsxPath)) return jsxPath;
+  if (fs.existsSync(tsxPath)) return tsxPath;
+
+  return null;
 }
-
-require('../dist/core/analyzer.js').default(entryPath);
