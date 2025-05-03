7cb5b7c1c77df90082efcdd6002cdd37163b3c52
김도현 <swputer@gmail.com>
2025-05-03 18:12:31 +0900

    feat: workspace 분리

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import path from 'node:path';
-
-import analyzer from '@/core/analyzer';
-
-const entryPath = path.resolve(__dirname, '../example/index.jsx');
-
-analyzer(entryPath);
-
-export * from '@/types';
