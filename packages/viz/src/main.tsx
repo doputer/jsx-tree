8b0a115e8d3921a0bc11cf68ff835bed5624b9be
김도현 <swputer@gmail.com>
2025-05-04 22:49:15 +0900

    feat: 스타일 초기화

--- packages/viz/src/main.tsx
+++ packages/viz/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from '@/App.tsx';
 
+import '@/styles/reset.css';
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
