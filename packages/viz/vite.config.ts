38617423d1b43d4cdfdba190d3c325ec3b6bc1f8
김도현 <swputer@gmail.com>
2025-05-04 22:46:37 +0900

    feat: CSS 라이브러리 추가

--- packages/viz/vite.config.ts
+++ packages/viz/vite.config.ts
@@ -1,11 +1,12 @@
 import { resolve } from 'node:path';
 
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vanillaExtractPlugin()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
   },
